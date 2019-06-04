e7a2f4e994ab770a7b2c0046ce18ffe2f25c29b1
ONELua Team <oneluaproject@gmail.com>
2019-06-04 09:09:16 -0500

    Update info

--- SHORTCUTS/git/shared.lua
+++ SHORTCUTS/git/shared.lua
@@ -5,7 +5,7 @@ APP_PROJECT = "Shortcuts-Enhancer"
 
 URL_WIKI = ""
 
-APP_VERSION_MAJOR = 0x01 -- major.minor
-APP_VERSION_MINOR = 0x01
+APP_VERSION_MAJOR = 0x02 -- major.minor
+APP_VERSION_MINOR = 0x00
 
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
